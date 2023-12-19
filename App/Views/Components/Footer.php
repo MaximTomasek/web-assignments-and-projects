c7f1483fb07b176eccf96d93a78115f63b39edf2
MaximTomasek <149430741+MaximTomasek@users.noreply.github.com>
2023-12-19 22:00:51 +0100

    Footer.PHP

---
+++ App/Views/Components/Footer.php
@@ -0,0 +1,38 @@
+<footer>
+        <div class="footer-container">
+            <div class="sec aboutus">
+                <h2>O mně</h2>
+                <p>Jsem vyučený kuchař a také mám maturitu v oboru podnikání. Je mi 23 let a na to že jsem docela mladý tak jsem si už vyzkoušel několik zaměstnání. Pracoval jsem jako prodejce v elektru, účetní a poradce životního pojištění. Od 15 let jsem chodil do posilovny do 18 let a poté ze zdravotních důvodů jsem musel přestat.</p>
+                <ul class="sci">
+                    <li><a href="#"><i class="bx bxl-facebook"></i></a></li>
+                    <li><a href="#"><i class="bx bxl-instagram"></i></a></li>
+                    <li><a href="#"><i class="bx bxl-twitter"></i></a></li>
+                    <li><a href="#"><i class="bx bxl-linkedin"></i></a></li>
+                </ul>
+            </div>
+            <div class="sec quicklinks">
+                <h2>Rychlé odkazy</h2>
+                <ul>
+                    <li><a href="#">Domů</a></li>
+                    <li><a href="#">Informace o mně</a></li>
+                    <li><a href="#">Ochrana osobních údajů</a></li>
+                    <li><a href="#">Podmínky použití</a></li>
+                    <li><a href="#">Cookies</a></li>
+                </ul>
+            </div>
+            <div class="sec contactBx">
+                <h2>Kontaktní informace</h2>
+                <ul class="info">
+                    <li>
+                        <span><i class='bx bxs-map'></i></span>
+                        <span>Kpt.Jaroše 2410 <br> Tábor, 39003 <br> CZ</span>
+                    </li>
+                    <li>
+                        <span><i class='bx bx-envelope' ></i></span>
+                        <p><a href="maximtomasek@gmail.com">maximtomasek@gmail.com</a></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <p style="text-align: center">&copy; 2023</p>
+    </footer>   
